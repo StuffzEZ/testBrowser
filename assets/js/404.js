8a0a548e78ab564868604b23a1e94e66f4289eaa
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2024-10-05 11:05:43 +0100

    bruh

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,61 +1,61 @@
 const currentUrl = window.location.href;
 
-if (currentUrl == "https://stuffzez.github.io/testBrowser/stuffzez"){
+if (currentUrl == "https://stuffzez.github.io/testBrowser//stuffzez"){
   location.replace("https://stuffzez.github.io")
 }
 
-if (currentUrl == "https://stuffzez.github.io/testBrowser/stuffzez-extras"){
+if (currentUrl == "https://stuffzez.github.io/testBrowser//stuffzez-extras"){
     location.replace("https://stuffzez.github.io")
 }
 
-if (currentUrl == "https://stuffzez.github.io/testBrowser/stuffzez-website"){
+if (currentUrl == "https://stuffzez.github.io/testBrowser//stuffzez-website"){
     location.replace("https://stuffzez.github.io")
 }
 
-if (currentUrl == "https://stuffzez.github.io/testBrowser/stiffezez"){
+if (currentUrl == "https://stuffzez.github.io/testBrowser//stiffezez"){
     location.replace("https://stuffzez.github.io")
 }
 
-if (currentUrl == "https://stuffzez.github.io/testBrowser/secret"){
+if (currentUrl == "https://stuffzez.github.io/testBrowser//secret"){
     location.replace("https://stuffzez.github.io/testBrowser/testbrowser/aprilfools")
 }
 
-if (currentUrl == "https://stuffzez.github.io/testBrowser/secret-plan"){
+if (currentUrl == "https://stuffzez.github.io/testBrowser//secret-plan"){
     location.replace("https://stuffzez.github.io/testBrowser/testbrowser/aprilfools")
 }
 
-if (currentUrl == "https://stuffzez.github.io/testBrowser/seecret"){
+if (currentUrl == "https://stuffzez.github.io/testBrowser//seecret"){
     location.replace("https://stuffzez.github.io/testBrowser/testbrowser/aprilfools")
 }
 
-if (currentUrl == "https://stuffzez.github.io/testBrowser/april-fools"){
+if (currentUrl == "https://stuffzez.github.io/testBrowser//april-fools"){
     location.replace("https://stuffzez.github.io/testBrowser/testbrowser/aprilfools")
 }
 
-if (currentUrl == "https://stuffzez.github.io/testBrowser/joke"){
+if (currentUrl == "https://stuffzez.github.io/testBrowser//joke"){
     location.replace("https://stuffzez.github.io/testBrowser/testbrowser/aprilfools")
 }
 
-if (currentUrl == "https://stuffzez.github.io/testBrowser/haha"){
+if (currentUrl == "https://stuffzez.github.io/testBrowser//haha"){
     location.replace("https://stuffzez.github.io/testBrowser/testbrowser/aprilfools")
 }
 
-if (currentUrl == "https://stuffzez.github.io/testBrowser/aprilfools"){
+if (currentUrl == "https://stuffzez.github.io/testBrowser//aprilfools"){
     location.replace("https://stuffzez.github.io/testBrowser/testbrowser/aprilfools")
 }
 
-if (currentUrl == "https://stuffzez.github.io/testBrowser/funny"){
+if (currentUrl == "https://stuffzez.github.io/testBrowser//funny"){
     location.replace("https://stuffzez.github.io/testBrowser/testbrowser/aprilfools")
 }
 
-if (currentUrl == "https://stuffzez.github.io/testBrowser/prank"){
+if (currentUrl == "https://stuffzez.github.io/testBrowser//prank"){
     location.replace("https://stuffzez.github.io/testBrowser/testbrowser/aprilfools")
 }
 
-if (currentUrl == "https://stuffzez.github.io/testBrowser/100000"){
+if (currentUrl == "https://stuffzez.github.io/testBrowser//100000"){
     location.replace("https://stuffzez.github.io/")
 }
 
-if (currentUrl == "https://stuffzez.github.io/testBrowser/538107"){
+if (currentUrl == "https://stuffzez.github.io/testBrowser//538107"){
     location.replace("https://stuffzez.github.io/testBrowser/testbrowser/aprilfools")
 }
